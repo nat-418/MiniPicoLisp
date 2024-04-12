942265e92ca56e8699bbe3acf3884e40dc7f7015
nat-418 <93013864+nat-418@users.noreply.github.com>
2024-04-12 21:37:27 +0200

    fix(picolisp): add Nix package

---
+++ package.nix
@@ -0,0 +1,33 @@
+{ lib, stdenv, gcc, gnumake }:
+
+let
+  crazyStr = (lib.strings.escapeShellArg "\${0%/*}");
+in
+
+stdenv.mkDerivation {
+  pname = "MiniPicoLisp";
+  version = "2018-09-30";
+  src = lib.sources.cleanSource ./.;
+
+  nativeBuildInputs = [ gcc gnumake ];
+
+  buildPhase = ''
+    rm *.nix
+    cd src
+    make
+    cd ..
+  '';
+
+  installPhase = ''
+    mkdir -p "$out"
+    cp -r . "$out"
+    ln -s "$out/mpil" "$out/bin/mpil"
+    substituteInPlace $out/bin/mpil --replace ${crazyStr} $out
+  '';
+
+  meta = {
+    description = ''An embeddable, "pure" PicoLisp interpreter.'';
+    homepage = "https://www.picolisp.com/wiki/?embedded";
+  };
+}
+
